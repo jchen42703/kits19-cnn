ee52c506b2559f5dca062806b97878980d831326
jchen42703 <jchen42703@gmail.com>
2019-04-07 22:40:45 -0400

    Added the add_bool_arg function to allow for args.extract to be a boolean
    This was to fix #2 . Now, extraction is defined by using either --extract or --no-extract. Also, I fixed args.dset_path's error by slicing (it was a list of a string rather than just a string).

--- scripts/download.py
+++ scripts/download.py
@@ -30,11 +30,27 @@ def download_kits19(file_id = "1NUZ7pyEActuaZ9sOyGbGUekL3X3ihVjn", extract = Tru
         print('Extracting data [DONE]')
     return None
 
+def add_bool_arg(parser, name, default=False):
+    """
+    From: https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+    Handles boolean cases from command line through the creating two mutually exclusive arguments: --name and --no-name.
+
+    Args:
+        parser (arg.parse.ArgumentParser): the parser you want to add the arguments to
+        name: name of the common feature name for the two mutually exclusive arguments; dest = name
+        default: default boolean for command line
+    Returns:
+        None
+    """
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--' + name, dest=name, action='store_true')
+    group.add_argument('--no-' + name, dest=name, action='store_false')
+    parser.set_defaults(**{name:default})
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='For downloading the KiTS 2019 Dataset')
-    parser.add_argument('--extract', nargs = 1, type = bool, required = False, default = True,
-                        help = 'Boolean on whether or not you want to extract the downloaded tar')
+    add_bool_arg(parser, "extract", default = True) # defaults to extract = True
     parser.add_argument('--dset_path', nargs = 1, type = str, required = False, default = "/content/",
                         help = 'Path to the base directory where you want to download and extract the tar file')
     args = parser.parse_args()
-    download_kits19(extract = args.extract, dset_path = args.dset_path)
+    download_kits19(extract = args.extract, dset_path = args.dset_path[0])
