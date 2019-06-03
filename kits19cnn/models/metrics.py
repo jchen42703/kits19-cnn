b58502bfcfd5192cbdd4c9a51307f3d3ec12c3a3
Joseph <jchen42703@gmail.com>
2019-06-03 19:06:54 -0400

    Added soft_dice and cleaned up `dice_plus_xent_loss`
    The soft dice function is based off of Isensee's nnU-Net Pytorch implementation and I adapted dice_plus_xent loss to make the code slimmer and cleaner.

--- kits19cnn/models/metrics.py
+++ kits19cnn/models/metrics.py
@@ -2,7 +2,8 @@
 import keras
 import tensorflow as tf
 import keras.backend as K
-def dice_plus_xent_loss(y_true, y_pred, smooth = 1e-5, out_act = 'sigmoid'):
+
+def dice_plus_xent_loss(y_true, y_pred, smooth=1e-5, out_act='sigmoid'):
     """
     Function to calculate the loss used in https://arxiv.org/pdf/1809.10486.pdf,
     no-new net, Isenseee et al (used to win the Medical Imaging Decathlon).
@@ -14,48 +15,38 @@ def dice_plus_xent_loss(y_true, y_pred, smooth = 1e-5, out_act = 'sigmoid'):
         the loss (cross_entropy + Dice)
     """
     y_pred = tf.cast(y_pred, tf.float32)
-    multi_class = y_pred.shape[-1] >= 2
     if out_act == "softmax":
-        loss_xent = K.mean(K.categorical_crossentropy(y_true, y_pred))
+        loss_xent = K.categorical_crossentropy(y_true, y_pred)
     elif out_act == "sigmoid":
-#         assert y_pred.shape[-1] == 1, "Please check that your outputted segmenatations are single channel \
-#                                        for binary cross entropy."
-        loss_xent = K.mean(K.binary_crossentropy(y_true, y_pred), axis = -1)
+        loss_xent = K.mean(K.binary_crossentropy(y_true, y_pred), axis=1)
     # Dice as according to the paper:
-    n_dims = len(y_pred.shape) - 2 # subtracting the batch_size and n_channels dimensions
-    axes = [axis for axis in range(n_dims + 1)] # to sum over all dimensions besides the channels (classes)
-    # axes = [0]
-    numerator = 2.0 * K.sum(y_true * y_pred, axis = axes)
-    denominator = K.sum(y_pred, axes) + K.sum(y_true, axes)
-    if multi_class:
-        loss_dice = K.mean((numerator + smooth) / (denominator + smooth))
-    elif not multi_class:
-        loss_dice = (numerator + smooth) / (denominator + smooth)
-    l_dice = tf.Print(loss_dice, [loss_dice], "dice: ")
-    l_xent = tf.Print(loss_xent, [loss_xent], "xent: ")
-#     l_xent = K.print_tensor(loss_xent, message = 'xent')
-#     l_dice = K.print_tensor(loss_dice, message = 'multi-class dice')
-#     pr_fn = tf.Print([loss_dice, loss_xent], [loss_dice, loss_xent], "soft dice: " + str(eval(loss_dice)) + "xent: " + str(eval(loss_xent)))
-    return -l_dice + l_xent
+    l_dice = soft_dice_loss(y_true, y_pred)
+    return l_dice + l_xent
 
-def multi_class_dice(y_true, y_pred, smooth = 1e-5):
+def soft_dice_loss(y_true, y_pred):
+    return -soft_dice(y_true, y_pred)
+
+def soft_dice(y_true, y_pred, smooth=1., smooth_in_numerator=1., square_numerator=False, square_denom=False):
     """
-    Simple multi-class dice coefficient that computes the average dice for each class.
-    This implementation assumes a "channels_last" tensor format.
+    Differentiable soft dice.
     Args:
         y_true:
         y_pred:
-        smooth: small value to avoid division by 0
-            default: 1e-5
+        smooth:
+        smooth_in_numerator:
+        square_numerator (bool): whether or not to the intersection of (y_true and y_pred) in the numerator
+        square_denominator (bool): whether or not to square y_true and y_pred in the denominator
     Returns:
-        The mean dice coefficient over all of the classes.
+        result: the calculated soft dice
     """
-    n_dims = len(y_pred.shape) - 2 # subtracting the batch_size and n_channels dimensions
-    axes = [axis for axis in range(n_dims + 1)] # to sum over all dimensions besides the channels (classes)
-    intersect = K.sum(y_true * y_pred, axis=axes)
-    numerator = 2 * intersect + smooth
-    denominator = K.sum(y_true, axis = axes) + K.sum(y_pred, axis = axes) + smooth
-    return K.mean(numerator / denominator)
-
-def multi_class_dice_loss(y_true, y_pred):
-    return -multi_class_dice(y_true, y_pred)
+    axes = tuple(range(2, len(y_true.size()))) # assumes channels_first
+    if square_numerator:
+        intersect = K.sum(y_pred * y_true, axes, keepdim=False)
+    else:
+        intersect = K.sum((y_pred * y_true) ** 2, axes, keepdim=False)
+    if square_denom:
+        denom = K.sum(y_pred ** 2 + y_true ** 2, axes, keepdim=False)
+    else:
+        denom = K.sum(y_pred + y_true, axes, keepdim=False)
+    result = K.mean(((2 * intersect + smooth_in_nom) / (denom + smooth)))
+    return result
