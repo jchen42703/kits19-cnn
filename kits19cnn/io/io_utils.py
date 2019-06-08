964b15851f8f6b1d2f4a18aaccc8957784e936f3
Joseph <jchen42703@gmail.com>
2019-06-08 13:28:19 -0400

    Added Resize, OneHot, ZeroMeanUnitVariance transforms to the list
    Also, allowed for customization for including the spatial data augmentation operations for faster development

--- kits19cnn/io/io_utils.py
+++ kits19cnn/io/io_utils.py
@@ -1,53 +1,42 @@
-from batchgenerators.transforms.color_transforms import ContrastAugmentationTransform
 from batchgenerators.transforms.spatial_transforms import MirrorTransform
-from batchgenerators.transforms.spatial_transforms import SpatialTransform
+from batchgenerators.transforms.spatial_transforms import SpatialTransform, ResizeTransform
+from batchgenerators.transforms.utility_transforms import ConvertMultiSegToOnehotTransform
+from batchgenerators.transforms.sample_normalization_transforms import ZeroMeanUnitVarianceTransform
 from batchgenerators.transforms.abstract_transforms import Compose
-# from batchgenerators.transforms.crop_and_pad_transforms import PositiveClassCropTransform
+import numpy as np
 
-def get_transforms(patch_shape = (80, 192, 128), pad_value = None, other_transforms = None, random_crop = False):
+def get_transforms(patch_shape=(80, 192, 128), other_transforms=None, minimal=True, random_crop=False):
     """
     Initializes the transforms for training.
     Args:
         patch_shape:
-        pad_value:
         other_transforms: List of transforms that you would like to add (optional). Defaults to None.
-        random_crop (boolean): whether or not you want to random crop or center crop. Currently, the Transformed3DGenerator
-        only supports random cropping. Transformed2DGenerator supports both random_crop = True and False.
+        minimal (boolean): whether to include more in-depth data aug (random rotations, random elastic deformations,
+        random crops, random zoom, mirroring) or not; True if no daug, False if yes more daug
+        random_crop (boolean): whether or not you want to random crop or center crop.
     """
-    # allowing for "nearest" padding
-    if pad_value is None:
-      border_mode = "nearest"
-      pad_value = 0
-    else:
-      border_mode = "constant"
-      border_cval_data = pad_value
+    resize =  ResizeTransform(patch_shape, concatenate_list=True)
+    mean_std_norm = ZeroMeanUnitVarianceTransform()
+    onehot = ConvertMultiSegToOnehotTransform([1, 2])
 
     ndim = len(patch_shape)
-    # brightness_transform = ContrastAugmentationTransform((0.3, 3.), preserve_range=True)
     spatial_transform = SpatialTransform(patch_shape,
-                     do_elastic_deform = True, alpha = (0., 1500.), sigma = (30., 50.),
-                     do_rotation = True, angle_z=(0, 2 * np.pi),
-                     do_scale = True, scale = (0.8, 2.),
-                     border_mode_data = border_mode, border_cval_data = pad_value,
-                     order_data = 1, random_crop = random_crop,
+                     do_elastic_deform=True, alpha=(0., 1500.), sigma=(30., 50.),
+                     do_rotation=True, angle_z=(0, 2 * np.pi),
+                     do_scale=True, scale=(0.8, 2.),
+                     border_mode_data="nearest",
+                     order_data=3, random_crop=random_crop,
                      p_el_per_sample=0.1, p_scale_per_sample=0.1, p_rot_per_sample=0.1)
     if ndim == 2:
         axes = (0, 1)
     elif ndim == 3:
         axes = (0, 1, 2)
     mirror_transform = MirrorTransform(axes = axes)
-    transforms_list = [spatial_transform, mirror_transform]
+    if minimal:
+        transforms_list = [resize, onehot, mean_std_norm]
+    else:
+        transforms_list = [resize, onehot, mean_std_norm, spatial_transform, mirror_transform]
     if other_transforms is not None:
         transforms_list = transforms_list + other_transforms
     composed = Compose(transforms_list)
     return composed
-
-def get_only_crop(patch_shape = (512, 512), random_crop = False):
-    """Returns a transform for only cropping"""
-    spatial_transform = SpatialTransform(patch_shape,
-                   do_elastic_deform = False,
-                   do_rotation = False,
-                   do_scale = False,
-                   border_mode_data = "nearest", border_cval_data = 0,
-                   order_data = 1, random_crop = random_crop)
-    return spatial_transform
