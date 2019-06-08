fadedb654ae319723de8f9ca73b9ddbdfac8ff3a
Joseph <jchen42703@gmail.com>
2019-06-08 11:55:40 -0400

    Update gen_utils.py
    Changed to tensorflow.keras, list_IDs to fpaths for clarity

--- kits19cnn/io/gen_utils.py
+++ kits19cnn/io/gen_utils.py
@@ -1,4 +1,4 @@
-import keras
+import tensorflow.keras as keras
 import numpy as np
 import os
 import nibabel as nib
@@ -8,19 +8,19 @@ class BaseGenerator(keras.utils.Sequence):
     Basic framework for generating thread-safe data in keras. (no preprocessing and channels_last)
     Based on https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
     Attributes:
-      list_IDs: filenames (.nii files); must be same for training and labels
+      fpaths: filenames (.nii files); must be same for training and labels
       batch_size: int of desired number images per epoch
       shuffle: boolean on whether or not to shuffle the dataset
     """
-    def __init__(self, list_IDs, batch_size, shuffle=True):
+    def __init__(self, fpaths, batch_size, shuffle=True):
         # lists of paths to images
-        self.list_IDs = list_IDs
+        self.fpaths = fpaths
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(len(self.fpaths))
 
     def __len__(self):
-        return int(np.ceil(len(self.list_IDs) / float(self.batch_size)))
+        return int(np.ceil(len(self.fpaths) / float(self.batch_size)))
 
     def __getitem__(self, idx):
         """
@@ -29,23 +29,23 @@ class BaseGenerator(keras.utils.Sequence):
         # file names
         indexes = self.indexes[idx*self.batch_size:(idx+1)*self.batch_size]
         # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        fpaths_temp = [self.fpaths[k] for k in indexes]
 
-        X, Y = self.data_gen(list_IDs_temp)
+        X, Y = self.data_gen(fpaths_temp)
         return (X, Y)
 
     def on_epoch_end(self):
-        'Updates indexes after each epoch'
+        """Updates indexes after each epoch"""
         # self.img_idx = np.arange(len(self.x))
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(len(self.fpaths))
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def data_gen(self, list_IDs_temp):
+    def data_gen(self, fpaths_temp):
         """
         Preprocesses the data
         Args:
-            list_IDs_temp: temporary batched list of ids (filenames)
+            fpaths_temp: temporary batched list of ids (filenames)
         Returns
             x, y
         """
@@ -55,19 +55,21 @@ class BaseTransformGenerator(BaseGenerator):
     """
     Loads data and applies data augmentation with `batchgenerators.transforms`.
     Attributes:
-        list_IDs: list of filenames
+        fpaths: list of filenames
         batch_size: The number of images you want in a single batch
         transform (Transform instance): If you want to use multiple Transforms, use the Compose Transform.
         steps_per_epoch: steps per epoch during training (number of samples per epoch = steps_per_epoch * batch_size )
         shuffle: boolean on whether to shuffle the dataset between epochs
     """
-    def __init__(self, list_IDs, batch_size, transform=None, steps_per_epoch=1000, shuffle=True):
+    def __init__(self, fpaths, batch_size, transform=None, steps_per_epoch=None, shuffle=True):
 
-        BaseGenerator.__init__(self, list_IDs=list_IDs, batch_size=batch_size,
+        BaseGenerator.__init__(self, fpaths=fpaths, batch_size=batch_size,
                                shuffle=shuffle)
         self.transform = transform
-        n_samples = len(self.list_IDs)
+        n_samples = len(self.fpaths)
         self.indexes = np.arange(n_samples)
+        if steps_per_epoch is None:
+            steps_per_epoch = n_samples
         n_idx = self.batch_size * steps_per_epoch # number of samples per epoch
         # Handles cases where the dataset is small and the batch size is high
         if n_idx > n_samples:
@@ -116,8 +118,8 @@ class BaseTransformGenerator(BaseGenerator):
         # file names
         indexes = self.indexes[idx*self.batch_size:(idx+1)*self.batch_size]
         # Fetches batched IDs for a thread
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-        X, Y = self.data_gen(list_IDs_temp)
+        fpaths_temp = [self.fpaths[k] for k in indexes]
+        X, Y = self.data_gen(fpaths_temp)
         if self.transform is not None:
             data_dict = {}
             data_dict["data"], data_dict["seg"] = X, Y
@@ -125,11 +127,11 @@ class BaseTransformGenerator(BaseGenerator):
             X, Y = data_dict["data"], data_dict["seg"]
         return (X, Y)
 
-    def data_gen(self, list_IDs_temp):
+    def data_gen(self, fpaths_temp):
         """
         Generates a batch of data.
         Args:
-            list_IDs_temp: batched list IDs; usually done by __getitem__
+            fpaths_temp: batched list IDs; usually done by __getitem__
             pos_sample: boolean on if you want to sample a positive image or not
         Returns:
             tuple of two numpy arrays: x, y
