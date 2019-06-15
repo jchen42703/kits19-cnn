55c33b9fec82aa8159e509661c669ed98bb942fd
Joseph <jchen42703@gmail.com>
2019-06-15 11:03:43 -0400

    Added the official evaluation metric
    From the official KiTS19 repository

--- kits19cnn/models/metrics.py
+++ kits19cnn/models/metrics.py
@@ -50,3 +50,27 @@ def soft_dice(y_true, y_pred, smooth=1., smooth_in_numerator=1., square_numerato
         denom = K.sum(y_pred + y_true, axes, keepdim=False)
     result = K.mean(((2 * intersect + smooth_in_nom) / (denom + smooth)))
     return result
+
+def evaluate_official(y_true, y_pred):
+    """
+    Official evaluation metric. (numpy)
+    """
+    try:
+        # Compute tumor+kidney Dice
+        tk_pd = np.greater(y_pred, 0)
+        tk_gt = np.greater(y_true, 0)
+        intersection = np.logical_and(tk_pd, tk_gt).sum()
+        tk_dice = 2*intersection/(tk_pd.sum() + tk_gt.sum())
+    except ZeroDivisionError:
+        return 0.0, 0.0
+
+    try:
+        # Compute tumor Dice
+        tu_pd = np.greater(y_pred, 1)
+        tu_gt = np.greater(y_true, 1)
+        intersection = np.logical_and(tu_pd, tu_gt).sum()
+        tu_dice = 2*intersection/(tu_pd.sum() + tu_gt.sum())
+    except ZeroDivisionError:
+        return tk_dice, 0.0
+
+    return tk_dice, tu_dice
