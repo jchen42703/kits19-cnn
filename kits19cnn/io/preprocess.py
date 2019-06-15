3689721c2706b48eb2dd808f6db8193afdd25074
jchen42703 <jchen42703@gmail.com>
2019-06-15 12:33:23 -0400

    self.cases now only contains "cases"
    Used to add the .json and LICENSE to the list, now it no longer does that

--- kits19cnn/io/preprocess.py
+++ kits19cnn/io/preprocess.py
@@ -34,10 +34,14 @@ class Preprocessor(object):
 
         if clip_values is None:
             self.clip_values = self.get_clip_values()
+        else:
+            self.clip_values = clip_values
+
         self.cases = cases
         if cases is None:
-            self.cases = os.listdir(self.in_dir)[:-2]
-            assert self.cases is not None, "Please make sure that in_dir refers to the kits19/data directory."
+            self.cases = [case for case in os.listdir(self.in_dir) \
+                          if case.startswith("case")]
+            assert len(self.cases) > 0, "Please make sure that in_dir refers to the kits19/data directory."
         # making directory if out_dir doesn't exist
         if not isdir(out_dir):
             os.mkdir(out_dir)
