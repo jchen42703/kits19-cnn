2f7cd9f2e3eb55fe3c1755cc53a9d284bda63084
Joseph <jchen42703@gmail.com>
2019-11-07 12:23:39 -0500

    Added the pred_3D_params argument and squeeze the test_batch
    Now works

--- kits19cnn/inference/inference_class.py
+++ kits19cnn/inference/inference_class.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 import numpy as np
 import inspect
 import nibabel as nib
+import torch
 
 from kits19cnn.inference.utils import load_weights_infer
 
@@ -11,18 +12,20 @@ class Predictor(object):
     Predictions are saved in `out_dir`.
     """
     def __init__(self, in_dir, out_dir, cases, checkpoint_path, model,
-                 test_loader):
+                 test_loader, pred_3D_params={"do_mirroring": True}):
         assert inspect.ismethod(model.predict_3D), \
                 "model must have the method `predict_3D`"
         self.model = load_weights_infer(checkpoint_path, model)
         self.test_loader = test_loader
+        self.pred_3D_params = pred_3D_params
 
     def run_3D_predictions(self):
         """
         Runs predictions on the dataset (specified in test_loader)
         """
         for test_batch in tqdm(self.test_loader):
-            pred, act = self.model.predict_3D(test_batch[0].cuda())
+            test_x = torch.squeeze(test_batch[0], dim=0)
+            pred, act = self.model.predict_3D(test_x, **self.pred_3D_params)
             assert len(pred.shape) == 3
             assert len(act.shape) == 4 # temp checks
             ### possible place to threshold ROI size ###
