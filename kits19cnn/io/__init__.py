65028155d5d1ecee1a1527a5199dacc331134118
jchen42703 <jchen42703@gmail.com>
2019-11-10 17:05:03 -0500

    Created SliceDataset
    For 2D data loading (each slice). Loads data created by io.Preprocessor.save_dir_as_2d()

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -1,4 +1,5 @@
 from .dataset import ClfSegVoxelDataset, VoxelDataset, TestVoxelDataset
+from .dataset_2d import SliceDataset
 from .preprocess import Preprocessor
 from .resample import resample_patient
 from .custom_transforms import ROICropTransform
