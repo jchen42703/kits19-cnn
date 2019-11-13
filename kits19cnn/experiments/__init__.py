fa1fb6585154eb18631092f388198c0a4e5ae6e2
jchen42703 <jchen42703@gmail.com>
2019-11-13 17:24:45 -0500

    Update __init__.py

--- kits19cnn/experiments/__init__.py
+++ kits19cnn/experiments/__init__.py
@@ -2,4 +2,5 @@ from .utils import get_training_augmentation, get_validation_augmentation, \
                    get_preprocessing, seed_everything
 from .train import TrainSegExperimentFromConfig, \
                    TrainClfSegExperimentFromConfig
+from .train_2d import TrainSeg2dExperimentFromConfig
 from .infer import SegmentationInferenceExperiment
