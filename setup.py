7d5ad55f1427f0d803b1bbffa897c6a9021add3a
jchen42703 <jchen42703@gmail.com>
2019-11-13 16:58:31 -0500

    version 0.01.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='kits19cnn',
-      version='0.01.3',
+      version='0.01.4',
       description='Submission for the KiTS 2019 Challenge',
       url='https://github.com/jchen42703/kits19-cnn',
       author='Joseph Chen, Benson Jin',
