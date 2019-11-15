f3617dd326a270aba5ef3d8e7052acfaad42072e
Joseph <jchen42703@gmail.com>
2019-11-15 10:06:54 -0500

    Added missing RepeatChannelsTransform

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -2,4 +2,4 @@ from .dataset import ClfSegVoxelDataset, VoxelDataset, TestVoxelDataset
 from .dataset_2d import SliceDataset
 from .preprocess import Preprocessor
 from .resample import resample_patient
-from .custom_transforms import ROICropTransform
+from .custom_transforms import ROICropTransform, RepeatChannelsTransform
