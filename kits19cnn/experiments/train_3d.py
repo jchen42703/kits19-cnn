ab8b8b2a9cac20d3ebae747294a29416f02a407a
Joseph <jchen42703@gmail.com>
2019-11-19 14:41:59 -0500

    Created Train3DClfSegExperimentFromConfig
    for 3D clf + seg cases

--- kits19cnn/experiments/train_3d.py
+++ kits19cnn/experiments/train_3d.py
@@ -1,9 +1,9 @@
 import torch
 
 from kits19cnn.models import Generic_UNet
-from kits19cnn.io import VoxelDataset
+from kits19cnn.io import VoxelDataset, ClfSegVoxelDataset
 
-from .train import TrainExperiment
+from .train import TrainExperiment, TrainClfSegExperimentFromConfig
 from .utils import get_preprocessing, get_training_augmentation, \
                   get_validation_augmentation, seed_everything
 
@@ -62,3 +62,42 @@ class TrainSegExperimentFromConfig(TrainExperiment):
         print(f"Total # of Params: {total}\nTrainable params: {trainable}")
 
         return model
+
+class Train3DClfSegExperimentFromConfig(TrainClfSegExperimentFromConfig, TrainSegExperimentFromConfig):
+    """
+    Stores the main parts of a classification+segmentation experiment:
+    - df split
+    - datasets
+    - loaders
+    - model
+    - optimizer
+    - lr_scheduler
+    - criterion
+    - callbacks
+    """
+    def __init__(self, config: dict):
+        """
+        Args:
+            config (dict): from `train_seg_yaml.py`
+        """
+        self.model_params = config["model_params"]
+        super().__init__(config=config)
+
+    def get_datasets(self, train_ids, valid_ids):
+        """
+        Creates and returns the train and validation datasets.
+        """
+        # preparing transforms
+        train_aug = get_training_augmentation(self.io_params["aug_key"])
+        val_aug = get_validation_augmentation(self.io_params["aug_key"])
+        # creating the datasets
+        preprocess = get_preprocessing()
+        train_dataset = ClfSegVoxelDataset(im_ids=train_ids,
+                                           transforms=train_aug,
+                                           preprocessing=preprocess,
+                                           mode="both", num_classes=3)
+        valid_dataset = ClfSegVoxelDataset(im_ids=valid_ids,
+                                           transforms=val_aug,
+                                           preprocessing=preprocess,
+                                           mode="both", num_classes=3)
+        return (train_dataset, valid_dataset)
