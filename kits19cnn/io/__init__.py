c7e64210474e46944e3920fd6de9f3f9ea7ee583
jchen42703 <jchen42703@gmail.com>
2019-12-01 16:54:10 -0500

    Added PseudoSliceDataset

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -1,5 +1,5 @@
 from .dataset import ClfSegVoxelDataset, VoxelDataset, TestVoxelDataset
-from .dataset_2d import SliceDataset
+from .dataset_2d import SliceDataset, PseudoSliceDataset
 from .preprocess import Preprocessor
 from .resample import resample_patient
 from .custom_transforms import ROICropTransform, RepeatChannelsTransform
