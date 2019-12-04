e4ef2b0d567b6d5ad390be697ab57df589eca2a1
Joseph <jchen42703@gmail.com>
2019-12-04 17:35:23 -0500

    Added support to specify the output activation function

--- kits19cnn/models/smp_models.py
+++ kits19cnn/models/smp_models.py
@@ -2,7 +2,8 @@ import torch
 from kits19cnn.models.nnunet.neural_network import SegmentationNetwork
 from kits19cnn.utils import softmax_helper
 
-def wrap_smp_model(smp_model_type, smp_model_kwargs={}, num_classes=3):
+def wrap_smp_model(smp_model_type, smp_model_kwargs={}, num_classes=3,
+                   activation="softmax"):
     """
     Wraps a 2D smp model with SegmentationNetwork's methods. Mainly for
     inference, so that the smp_model can use the `predict_3D` method.
@@ -12,6 +13,9 @@ def wrap_smp_model(smp_model_type, smp_model_kwargs={}, num_classes=3):
             super().__init__(**model_kwargs)
             self.conv_op = torch.nn.Conv2d
             self.num_classes = num_classes
-            self.inference_apply_nonlin = softmax_helper
+            if activation == "softmax":
+                self.inference_apply_nonlin = softmax_helper
+            elif activation == "sigmoid":
+                self.inference_apply_nonlin = torch.sigmoid
     wrapped_model = WrappedModel(smp_model_kwargs)
     return wrapped_model
