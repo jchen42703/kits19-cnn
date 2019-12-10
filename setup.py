c80d1b6d3b91f499fd5dd0c2e31d57c6b1ca2a13
jchen42703 <jchen42703@gmail.com>
2019-12-09 23:32:32 -0500

    Updating for segmentation_models_pytorch==0.1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='kits19cnn',
-      version='0.01.5',
-      description='Submission for the KiTS 2019 Challenge',
-      url='https://github.com/jchen42703/kits19-cnn',
-      author='Joseph Chen, Benson Jin',
-      author_email='jchen42703@gmail.com, jinb2@bxsci.edu',
-      license='Apache License Version 2.0, January 2004',
+setup(name="kits19cnn",
+      version="0.01.5",
+      description="Submission for the KiTS 2019 Challenge",
+      url="https://github.com/jchen42703/kits19-cnn",
+      author="Joseph Chen, Benson Jin",
+      author_email="jchen42703@gmail.com, jinb2@bxsci.edu",
+      license="Apache License Version 2.0, January 2004",
       packages=find_packages(),
       install_requires=[
             "numpy>=1.10.2",
@@ -23,20 +23,17 @@ setup(name='kits19cnn',
             "torchvision>=0.4.0",
             "catalyst",
             "pytorch_toolbelt",
-            "segmentation_models_pytorch",
+            "segmentation_models_pytorch==0.1.0",
       ],
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          "Development Status :: 3 - Alpha",
           # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-          'Intended Audience :: Developers',  # Define that your audience are developers
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',  # Again, pick a license
-          'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 2.7',
+          "Intended Audience :: Developers",  # Define that your audience are developers
+          "Topic :: Software Development :: Build Tools",
+          "License :: OSI Approved :: MIT License",  # Again, pick a license
+          "Programming Language :: Python :: 3",  # Specify which python versions that you want to support
+          "Programming Language :: Python :: 3.6",
       ],
-      keywords=['deep learning', 'image segmentation', 'image classification', 'medical image analysis',
-                  'medical image segmentation', 'data augmentation'],
+      keywords=["deep learning", "image segmentation", "image classification", "medical image analysis",
+                 "medical image segmentation", "data augmentation"],
       )
